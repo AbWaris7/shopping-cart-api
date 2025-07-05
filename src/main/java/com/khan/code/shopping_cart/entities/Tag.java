0170e6de9f02f34d6021eacbc6a5f67c34130076
abwaris7 <hellokhan.ch@gmail.com>
2025-07-05 14:18:26 +0200

    added Tag Entity

---
+++ src/main/java/com/khan/code/shopping_cart/entities/Tag.java
@@ -0,0 +1,23 @@
+package com.khan.code.shopping_cart.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+}
