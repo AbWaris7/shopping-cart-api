85d810a7e7f866d28b9dcd6c6c7e0a18b7295d2f
abwaris7 <hellokhan.ch@gmail.com>
2025-07-05 14:18:10 +0200

    added Address Entity

---
+++ src/main/java/com/khan/code/shopping_cart/entities/Address.java
@@ -0,0 +1,29 @@
+package com.khan.code.shopping_cart.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "zip")
+    private String zip;
+    @Column(name = "state")
+    private String state;
+
+
+}
