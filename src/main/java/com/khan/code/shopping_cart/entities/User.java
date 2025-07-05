f434b269a2b066d2ddad5d5b8324dd212baeed12
abwaris7 <hellokhan.ch@gmail.com>
2025-07-05 13:50:14 +0200

    added User Entity with its fields

---
+++ src/main/java/com/khan/code/shopping_cart/entities/User.java
@@ -0,0 +1,28 @@
+package com.khan.code.shopping_cart.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+
+}
